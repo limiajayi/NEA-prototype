ad77579fe9521a0095b2a520b9e9959082641c65
limiajayi <limiajayi@gmail.com>
2023-09-17 19:52:18 +0100

    Small changes to mathsTopics views

--- visualmaths/mathsTopics/views.py
+++ visualmaths/mathsTopics/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from students.models import Question
 
 # Create your views here.
 
 def quadratics(request):
     return render(request, 'mathsTopics/quadratics.html')
+
+def quadraticsQs(request):
+    Question.objects.filter(subject='Maths', topic='Quadratics', difficulty='Easy')
